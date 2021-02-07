eb0a4fd097700aeaed10aef804671f446fd5e992
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2021-02-07 17:54:10 +0100

    Fixed config reloading

--- src/main.c
+++ src/main.c
@@ -333,56 +333,23 @@ error_exit:
     return err;
 }
 
-int main(int argc, char* argv[])
+int init(char configpath[], struct udevInfo* udev, struct configInfo* config, struct keyboardInfo* kbd, struct managedBuffer* device, int* epollfd, struct epoll_event* udevevent)
 {
-    struct argInfo arg;
-    struct udevInfo udev;
-    struct configInfo config;
-    struct keyboardInfo kbd;
-
-    struct managedBuffer device;
-
-    int epollfd;
-    struct epoll_event udevevent;
-
     // reset global variables
     g_brexit = false;
     g_reloadconfig = false;
-    g_daemonize = false;
-    g_loglevel = 0;
-
-    // handle non root
-    if (getuid() != 0) {
-        LOG(-1, "Please restart this daemon as root!\n");
-        return -1;
-    }
-
-    // set coredump limit
-    {
-        struct rlimit limit;
-        limit.rlim_cur = 0;
-        limit.rlim_max = 0;
-
-        setrlimit(RLIMIT_CORE, &limit);
-    }
 
     // init device managed buffer
-    m_init(&device, sizeof(struct deviceInfo));
-
-    // interpret args
-    if (handleargs(argc, argv, &arg)) {
-        LOG(-1, "handleargs failed!\n");
-        return -1;
-    }
+    m_init(device, sizeof(struct deviceInfo));
 
     // read config
-    if (readconfig(arg.configpath, &config)) {
+    if (readconfig(configpath, config)) {
         LOG(-1, "readconfig failed\n");
         return -1;
     }
     // daemonize if supplied
     if (g_daemonize) {
-        if (become_daemon(config)) {
+        if (become_daemon(*config)) {
             LOG(-1, "become_daemon failed!\n");
             return -1;
         }
@@ -399,33 +366,77 @@ int main(int argc, char* argv[])
     }
 
     // init the udev monitor
-    if (init_udev(&udev)) {
+    if (init_udev(udev)) {
         LOG(-1, "init_udev failed\n");
         return -1;
     }
 
     // init keylogging if supplied
-    if (init_keylogging(NULL, &kbd, &config)) {
+    if (init_keylogging(NULL, kbd, config)) {
         LOG(-1, "init_keylogging failed\n");
         return -1;
     }
 
     // SETUP EPOLL
-    epollfd = epoll_create(1); // size gets ignored since kernel 2.6.8
-    if (epollfd < 0) {
+    *epollfd = epoll_create(1); // size gets ignored since kernel 2.6.8
+    if (*epollfd < 0) {
         ERR("epoll_create");
         return -1;
     }
 
-    memset_s(&udevevent, sizeof(struct epoll_event), 0);
-    udevevent.events = EPOLLIN;
-    udevevent.data.fd = udev.udevfd;
+    memset_s(udevevent, sizeof(struct epoll_event), 0);
+    udevevent->events = EPOLLIN;
+    udevevent->data.fd = udev->udevfd;
 
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, udev.udevfd, &udevevent)) {
+    if (epoll_ctl(*epollfd, EPOLL_CTL_ADD, udev->udevfd, udevevent)) {
         ERR("epoll_ctl");
         return -1;
     }
     LOG(0, "Startup done!\n");
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+    struct argInfo arg;
+    struct udevInfo udev;
+    struct configInfo config;
+    struct keyboardInfo kbd;
+
+    struct managedBuffer device;
+
+    int epollfd = 0;
+    struct epoll_event udevevent;
+
+    // handle non root
+    if (getuid() != 0) {
+        LOG(-1, "Please restart this daemon as root!\n");
+        return -1;
+    }
+
+    // set coredump limit
+    {
+        struct rlimit limit;
+        limit.rlim_cur = 0;
+        limit.rlim_max = 0;
+
+        setrlimit(RLIMIT_CORE, &limit);
+    }
+
+    g_daemonize = false;
+    g_loglevel = 0;
+
+    // interpret args
+    if (handleargs(argc, argv, &arg)) {
+        LOG(-1, "handleargs failed!\n");
+        return -1;
+    }
+
+    // initalize the daemon contexts
+    if (init(arg.configpath, &udev, &config, &kbd, &device, &epollfd, &udevevent)) {
+        LOG(-1, "init failed\n");
+        return -1;
+    }
 
     // MAIN LOOP
     while (!g_brexit) {
@@ -493,12 +504,13 @@ int main(int argc, char* argv[])
 
         // reload config if SIGHUP is received
         if (g_reloadconfig) {
-            LOG(0, "Reloading config file...\n");
+            LOG(0, "Reloading config file...\n\n");
 
-            if (readconfig(arg.configpath, &config)) {
-                LOG(-1, "readconfig failed\n");
+            if (init(arg.configpath, &udev, &config, &kbd, &device, &epollfd, &udevevent)) {
+                LOG(-1, "init failed\n");
                 return -1;
             }
+
             g_reloadconfig = false;
         }
     }
