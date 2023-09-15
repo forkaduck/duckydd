ea3a14c62fa4f843979a2cca4d46e3e725bf7fc1
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2023-09-15 11:44:57 +0200

    Switched to toml type configuration file

--- src/io.c
+++ src/io.c
@@ -1,9 +1,11 @@
 #include <inttypes.h>
 #include <limits.h>
+#include <safe_str_lib.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #include <errno.h>
 #include <fcntl.h>
@@ -17,6 +19,7 @@
 #include "io.h"
 #include "mbuffer.h"
 #include "safe_lib.h"
+#include "toml.h"
 
 #define PREFIX long_int
 #define T long int
@@ -30,343 +33,280 @@
 #define T char
 #include "mbuffertemplate.h"
 
-// config interpretation functions
-static void cleaninput(char* input, const size_t size)
+#define MIN(i, k) (((i) < (k)) ? (i) : (k))
+
+int readconfig(const char path[], struct configInfo *config)
 {
-    size_t i, offset = 0;
+	int fd_config;
 
-    for (i = 0; i < size; i++) {
-        const char curr = input[i - offset];
+	config->maxcount = -1;
+	config->logpath[0] = '\0';
+	config->xkeymaps = false;
+	config->minavrg.tv_sec = 0;
+	config->minavrg.tv_nsec = 0;
 
-        // check for invalid characters
-        if ((curr <= '9' && curr >= '0') || (curr <= 'Z' && curr >= 'A') || (curr <= 'z' && curr >= 'a') || curr == '/' || curr == ' ') {
-            input[i - offset] = curr;
+	// Open the config file as read-only.
+	fd_config = open(path, O_RDWR);
+	if (fd_config < 0) {
+		ERR("open");
+		return -1;
+	}
 
-        } else if (curr == '\0' || curr == '\n') {
-            input[i - offset] = '\0';
+	// Try to lock the file so that only one instance of the daemon
+	// can be run at a given time.
+	{
+		struct flock lock;
+		lock.l_type = F_WRLCK;
+		lock.l_whence = SEEK_SET;
 
-        } else {
-            offset++;
-        }
-    }
-    input[i] = '\0';
+		lock.l_start = 0;
+		lock.l_len = 0;
+
+		if (fcntl(fd_config, F_SETLK, &lock)) {
+			if (errno == EACCES || errno == EAGAIN) {
+				LOG(-1,
+				    "Another instance is probably running!\n");
+			}
+			ERR("fcntl");
+			return -1;
+		}
+	}
+
+	{
+		FILE *p_config;
+		char err_ret_buff[200];
+
+		// Convert the file descriptor to a FILE pointer.
+		p_config = fdopen(fd_config, "r");
+		if (!p_config) {
+			ERR("fdopen");
+			return -1;
+		}
+
+		// Parse the configuration file and extract all values from the "config" table.
+		toml_table_t *content = toml_parse_file(p_config, err_ret_buff,
+							sizeof(err_ret_buff));
+		if (!content) {
+			ERR("toml_parse_file");
+			return -1;
+		}
+
+		const toml_table_t *config_table =
+			toml_table_in(content, "config");
+		if (!config_table) {
+			ERR("toml_table_in");
+			return -1;
+		}
+
+		// Handle all possible configuration entries.
+		const toml_datum_t minimum_avg =
+			toml_int_in(config_table, "minimum_avg");
+		if (minimum_avg.ok) {
+			config->minavrg.tv_nsec = minimum_avg.u.i;
+		}
+
+		const toml_datum_t max_score =
+			toml_int_in(config_table, "max_score");
+		if (max_score.ok) {
+			config->maxcount = max_score.u.i;
+		}
+
+		const toml_datum_t use_xkeymaps =
+			toml_bool_in(config_table, "use_xkeymaps");
+		if (use_xkeymaps.ok) {
+			config->xkeymaps = use_xkeymaps.u.b;
+		}
+
+		const toml_datum_t daemon_log_path =
+			toml_string_in(config_table, "daemon_log_path");
+		if (daemon_log_path.ok) {
+			strcpy_s(config->logpath, PATH_MAX,
+				 daemon_log_path.u.s);
+		}
+
+		toml_free(content);
+	}
+
+	if (close(fd_config)) {
+		ERR("close");
+	}
+	return 0;
 }
 
-static long int parse_long(const char input[], char** end)
+int handleargs(int argc, char *argv[], struct argInfo *data)
 {
-    long int num;
-    errno = 0;
+	int i;
+	bool unrecognized = false;
+	bool help = false;
+	data->configpath[0] = '\0';
 
-    num = strtol(input, end, 0);
-    if (end != NULL) {
-        if (*end == input) {
-            return -2;
-        }
-    }
+	for (i = 1; i < argc; i++) {
+		if (argv[i][0] == '-') {
+			switch (argv[i][1]) {
+			// path to the config to be used
+			case 'c':
+				if (i + 1 < argc) {
+					strcpy_s(data->configpath, PATH_MAX,
+						 argv[i + 1]); // config path
+				}
+				break;
 
-    // if the value is out of the range of a long
-    if ((num == LLONG_MAX || num == LLONG_MIN) && errno == ERANGE) {
-        return -3;
-    }
+			// daemonize the daemon
+			case 'd':
+				g_daemonize = true;
+				break;
 
-    return num;
-}
+			// increase the verbosity level
+			case 'v':
+				if (g_loglevel < MAX_LOGLEVEL) {
+					g_loglevel++;
+				} else {
+					LOG(0,
+					    "Can't increment loglevel any more!\n");
+				}
+				break;
 
-int readconfig(const char path[], struct configInfo* config)
-{
-    int fd;
-
-    config->maxcount = -1;
-    config->logpath[0] = '\0';
-    config->xkeymaps = false;
-    config->minavrg.tv_sec = 0;
-    config->minavrg.tv_nsec = 0;
-
-    // open the config file if it has no lock on it
-    fd = open(path, O_RDWR); // open the config
-    if (fd < 0) {
-        ERR("open");
-        return -1;
-    }
-
-    // try to lock the file if possible
-    {
-        struct flock lock;
-        lock.l_type = F_WRLCK;
-        lock.l_whence = SEEK_SET;
-
-        lock.l_start = 0;
-        lock.l_len = 0;
-
-        if (fcntl(fd, F_SETLK, &lock)) {
-            if (errno == EACCES || errno == EAGAIN) {
-                LOG(-1, "Another instance is probably running!\n");
-            }
-            ERR("fcntl");
-            return -2;
-        }
-    }
-
-    {
-        int rv = 1;
-
-        const size_t buffsize = 200;
-        char buff[buffsize];
-
-        lseek(fd, 0, SEEK_SET);
-        while (rv > 0) {
-            int i;
-
-            memset_s(buff, buffsize, 0);
-            rv = read(fd, &buff, buffsize);
-            if (rv < 0) {
-                ERR("read");
-                return -3;
-            }
-
-            for (i = 0; i < rv; i++) {
-                if (buff[i] == '\n' || buff[i] == '\0') {
-                    lseek(fd, i + 1 - rv, SEEK_CUR);
-
-                    // cleanup string
-                    cleaninput(buff, i);
-                    LOG(2, "-> %s rv: %d\n", buff, rv);
-                    break;
-                }
-            }
-
-            // gets the minimal average time difference between keystrokes
-            if (strncmp_ss(buff, "minavrg", 6) == 0) {
-                char* end = NULL;
-
-                config->minavrg.tv_sec = parse_long(&buff[8], &end);
-                config->minavrg.tv_nsec = parse_long(&end[1], &end);
-
-                if (config->minavrg.tv_sec < 0 || config->minavrg.tv_nsec < 0) {
-                    LOG(-1, "The option of minavrg is malformed or out of range!\n");
-                    return -4;
-                }
-
-                LOG(1, "Minavrg set to %lds %ldns\n", config->minavrg.tv_sec, config->minavrg.tv_nsec);
-
-            } else if (strncmp_ss(buff, "maxscore", 7) == 0) {
-                // sets the max score at which the device will be locked
-                config->maxcount = parse_long(&buff[9], NULL);
-
-                if (config->maxcount < 0) {
-                    LOG(-1, "The option of maxscore is malformed or out of range!\n");
-                    return -5;
-                }
-
-                LOG(1, "Maxscore set to %ld\n", config->maxcount);
-
-            } else if (strncmp_ss(buff, "logpath", 6) == 0) {
-                struct stat st;
-
-                // path where the logfile will be saved
-                pathcpy(config->logpath, &buff[8]);
-
-                if (stat(config->logpath, &st) < 0) {
-                    if (errno == ENOENT) {
-                        if (mkdir(config->logpath, 731)) {
-                            ERR("mkdir");
-                            return -6;
-                        }
-                        LOG(0, "Created logging directory!\n");
-
-                    } else {
-                        ERR("stat");
-                        return -7;
-                    }
-
-                } else {
-                    if (S_ISDIR(st.st_mode)) {
-                        LOG(1, "Set %s as the path for logging!\n", config->logpath);
-
-                    } else {
-                        LOG(-1, "Logpath does not point to a directory!\n");
-                        return -8;
-                    }
-                }
-            } else if (strncmp_ss(buff, "usexkeymaps", 10) == 0) {
-                // enables the use of x server keymaps if they are available
-                if (parse_long(&buff[12], NULL) == 1) {
-                    config->xkeymaps = true;
-                    LOG(1, "UseXKeymaps set to 1!\n");
-                }
-            }
-        }
-    }
-    if (close(fd)) {
-        ERR("close");
-    }
-    return 0;
-}
-
-int handleargs(int argc, char* argv[], struct argInfo* data)
-{
-    int i;
-    bool unrecognized = false;
-    bool help = false;
-    data->configpath[0] = '\0';
-
-    for (i = 1; i < argc; i++) {
-        if (argv[i][0] == '-') {
-            switch (argv[i][1]) {
-            // path to the config to be used
-            case 'c':
-                if (i + 1 < argc) {
-                    strcpy_s(data->configpath, PATH_MAX, argv[i + 1]); // config path
-                }
-                break;
-
-            // daemonize the daemon
-            case 'd':
-                g_daemonize = true;
-                break;
-
-            // increase the verbosity level
-            case 'v':
-                if (g_loglevel < MAX_LOGLEVEL) {
-                    g_loglevel++;
-                } else {
-                    LOG(0, "Can't increment loglevel any more!\n");
-                }
-                break;
-
-            // shows help
-            case 'h':
-                printf("duckydd %s\n"
-                       "Usage: duckydd [Options]\n"
-                       "\t\t-c <file>\tSpecify a config file path\n"
-                       "\t\t-d\t\tDaemonize the process\n"
-                       "\t\t-v\t\tIncrease verbosity of the console output (The maximum verbosity is 2)\n"
-                       "\t\t\t\tTHE -v OPTION CAN POTENTIALY EXPOSE PASSWORDS!!!\n"
-                       "\t\t-h\t\tShows this help section\n\n"
-                       "For config options please have a look at the README.md\n"
-                       "The daemon was linked against: udev "
+			// shows help
+			case 'h':
+				printf("duckydd %s\n"
+				       "Usage: duckydd [Options]\n"
+				       "\t\t-c <file>\tSpecify a config file path\n"
+				       "\t\t-d\t\tDaemonize the process\n"
+				       "\t\t-v\t\tIncrease verbosity of the console output (The maximum verbosity is 2)\n"
+				       "\t\t\t\tTHE -v OPTION CAN POTENTIALY EXPOSE PASSWORDS!!!\n"
+				       "\t\t-h\t\tShows this help section\n\n"
+				       "For config options please have a look at the README.md\n"
+				       "The daemon was linked against: udev "
 #ifdef ENABLE_XKB_EXTENSION
-                       "xkbcommon xkbcommon-x11 xcb "
+				       "xkbcommon xkbcommon-x11 xcb "
 #endif
-                       "\n",
-                    GIT_VERSION);
-                help = true;
-                break;
+				       "\n",
+				       GIT_VERSION);
+				help = true;
+				break;
 
-            default:
-                LOG(0, "%s is not a recognized option. \n", argv[i]);
-                unrecognized = true;
-                break;
-            }
-        }
-    }
+			default:
+				LOG(0, "%s is not a recognized option. \n",
+				    argv[i]);
+				unrecognized = true;
+				break;
+			}
+		}
+	}
 
-    if (unrecognized) {
-        LOG(0, "One or more options where not recognized! You can try the -h argument for a list of supported options.\n");
-    }
+	if (unrecognized) {
+		LOG(0,
+		    "One or more options where not recognized! You can try the -h argument for a list of supported options.\n");
+	}
 
-    if (help) {
-        return -1;
-    } else if (data->configpath[0] == '\0') {
-        LOG(0, "Please provide a config location!\n");
-        return -1;
-    }
-    return 0;
+	if (help) {
+		return -1;
+	} else if (data->configpath[0] == '\0') {
+		LOG(0, "Please provide a config location!\n");
+		return -1;
+	}
+	return 0;
 }
 
 void _logger(short loglevel, const char func[], const char format[], ...)
 {
-    // check for a format string bigger than the max
-    if (loglevel <= g_loglevel) {
-        if (strnlen(func, MAX_SIZE_FORMAT_STRING) <= MAX_SIZE_FORMAT_STRING
-            && strnlen(format, MAX_SIZE_FUNCTION_NAME) <= MAX_SIZE_FUNCTION_NAME) {
+	// check for a format string bigger than the max
+	if (loglevel <= g_loglevel) {
+		if (strnlen(func, MAX_SIZE_FORMAT_STRING) <=
+			    MAX_SIZE_FORMAT_STRING &&
+		    strnlen(format, MAX_SIZE_FUNCTION_NAME) <=
+			    MAX_SIZE_FUNCTION_NAME) {
+			va_list args;
+			va_start(args, format);
 
-            va_list args;
-            va_start(args, format);
+			char appended[MAX_SIZE_FORMAT_STRING +
+				      MAX_SIZE_FUNCTION_NAME];
+			char prefix;
+			FILE *fd;
 
-            char appended[MAX_SIZE_FORMAT_STRING + MAX_SIZE_FUNCTION_NAME];
-            char prefix;
-            FILE* fd;
+			// change prefix depending on loglevel
+			switch (loglevel) {
+			case -1:
+				prefix = '!';
+				fd = stderr;
+				break;
 
-            // change prefix depending on loglevel
-            switch (loglevel) {
-            case -1:
-                prefix = '!';
-                fd = stderr;
-                break;
+			default:
+				prefix = '*';
+				fd = stdout;
+				break;
+			}
 
-            default:
-                prefix = '*';
-                fd = stdout;
-                break;
-            }
+			if (g_loglevel > 0) {
+				sprintf(appended, "[%c][%s] %s", prefix, func,
+					format);
+			} else {
+				sprintf(appended, "[%c] %s", prefix, format);
+			}
+			vfprintf(fd, appended, args);
 
-            if (g_loglevel > 0) {
-                sprintf(appended, "[%c][%s] %s", prefix, func, format);
-            } else {
-                sprintf(appended, "[%c] %s", prefix, format);
-            }
-            vfprintf(fd, appended, args);
-
-            va_end(args);
-        }
-    }
+			va_end(args);
+		}
+	}
 }
 
 errno_t pathcat(char path1[], const char path2[])
 {
-    if (strnlen_s(path1, PATH_MAX) + strnlen_s(path2, PATH_MAX) < PATH_MAX) {
-        return strcat_s(path1, PATH_MAX, path2);
-    }
-    return EINVAL;
+	if (strnlen_s(path1, PATH_MAX) + strnlen_s(path2, PATH_MAX) <
+	    PATH_MAX) {
+		return strcat_s(path1, PATH_MAX, path2);
+	}
+	return EINVAL;
 }
 
 errno_t pathcpy(char path1[], const char path2[])
 {
-    if (strnlen_s(path1, PATH_MAX) + strnlen_s(path2, PATH_MAX) < PATH_MAX) {
-        return strcpy_s(path1, PATH_MAX, path2);
-    }
-    return EINVAL;
+	if (strnlen_s(path1, PATH_MAX) + strnlen_s(path2, PATH_MAX) <
+	    PATH_MAX) {
+		return strcpy_s(path1, PATH_MAX, path2);
+	}
+	return EINVAL;
 }
 
 // memsave strcmp functions
 int strcmp_ss(const char str1[], const char str2[])
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (str1[i] == str2[i]) {
-        if (str1[i] == '\0' || str2[i] == '\0') {
-            break;
-        }
-        i++;
-    }
+	while (str1[i] == str2[i]) {
+		if (str1[i] == '\0' || str2[i] == '\0') {
+			break;
+		}
+		i++;
+	}
 
-    return str1[i] - str2[i];
+	return str1[i] - str2[i];
 }
 
 int strncmp_ss(const char str1[], const char str2[], size_t length)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-    while (str1[i] == str2[i] && i < length) {
-        if (str1[i] == '\0' || str2[i] == '\0') {
-            break;
-        }
-        i++;
-    }
+	while (str1[i] == str2[i] && i < length) {
+		if (str1[i] == '\0' || str2[i] == '\0') {
+			break;
+		}
+		i++;
+	}
 
-    return str1[i] - str2[i];
+	return str1[i] - str2[i];
 }
 
 // returns the filename from a path
-const char* find_file(const char* input)
+const char *find_file(const char *input)
 {
-    size_t i;
+	size_t i;
 
-    for (i = strnlen_s(input, PATH_MAX); i > 0; i--) { // returns the filename
-        if (input[i] == '/') {
-            return &input[i + 1];
-        }
-    }
-    return NULL;
+	for (i = strnlen_s(input, PATH_MAX); i > 0;
+	     i--) { // returns the filename
+		if (input[i] == '/') {
+			return &input[i + 1];
+		}
+	}
+	return NULL;
 }
