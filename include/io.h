aef234313b3cc6e09b9c2d7f1af0976546569467
0xDEADC0DEx <48600374+0xDEADC0DEx@users.noreply.github.com>
2023-09-22 12:05:26 +0200

    Reformated

--- include/io.h
+++ include/io.h
@@ -11,31 +11,34 @@
 #include "mbuffer.h"
 #include "vars.h"
 
-#define LOG(loglvl, format, args...) _logger(loglvl, __func__, format, ##args) // print function name
-#define ERR(function) _logger(-1, __func__, "%s has failed (%d) -> %s (err: %d)\n", function, errno, strerror(errno))
+#define LOG(loglvl, format, args...)                                           \
+	_logger(loglvl, __func__, format, ##args) // print function name
+#define ERR(function)                                                          \
+	_logger(-1, __func__, "%s has failed (%d) -> %s (err: %d)\n",          \
+		function, errno, strerror(errno))
 
 // holds data read from the config file (mainly used by readconfig)
 struct configInfo {
-    long int maxcount;
+	long int maxcount;
 
-    char logpath[PATH_MAX];
+	char logpath[PATH_MAX];
 
-    // kbd
-    bool xkeymaps;
+	// kbd
+	bool xkeymaps;
 
-    struct timespec minavrg;
+	struct timespec minavrg;
 };
 
 // holds data that was parsed out by handleargs
 struct argInfo {
-    char configpath[PATH_MAX];
+	char configpath[PATH_MAX];
 };
 
-int readconfig(const char path[], struct configInfo* data);
-int handleargs(int argc, char* argv[], struct argInfo* data);
+int readconfig(const char path[], struct configInfo *data);
+int handleargs(int argc, char *argv[], struct argInfo *data);
 
 // internal logger function
-char* binexpand(uint8_t bin, size_t size);
+char *binexpand(uint8_t bin, size_t size);
 void _logger(short loglevel, const char func[], const char format[], ...);
 
 errno_t pathcat(char path1[], const char path2[]);
@@ -45,5 +48,5 @@ errno_t pathcpy(char path1[], const char path2[]);
 int strcmp_ss(const char str1[], const char str2[]);
 int strncmp_ss(const char str1[], const char str2[], size_t length);
 
-const char* find_file(const char* input);
+const char *find_file(const char *input);
 #endif
